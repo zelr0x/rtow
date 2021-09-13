ddac15024cd09b5fbb3e4d987fb8035b86fbec63
zelr0x <zelr0xdev@gmail.com>
2021-09-14 03:45:05 +0400

    Progress indicator

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-fn main() {
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
     let image_width = 256;
     let image_height = 256;
     println!("P3");
     println!("{} {}", image_width, image_height);
     println!("255");
     for j in (0..=image_height-1).rev() {
+        io::stderr().write(
+            format!("\rScanlines remaining: {}", j)
+            .as_bytes())?;
+        io::stderr().flush()?;
         for i in 0..image_width {
             let r = i as f64 / (image_width as f64 - 1.0);
             let g = j as f64 / (image_height as f64 - 1.0);
@@ -15,4 +21,6 @@ fn main() {
             println!("{} {} {}", ir, ig, ib);
         }
     }
+    io::stderr().write(b"\nDone.\n")?;
+    Ok(())
 }
